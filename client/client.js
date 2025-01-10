aca90aca5e1846cfb0e38f969f782a25f5fe868f
yuvrajkengar2539 <162606922+yuvrajkengar2539@users.noreply.github.com>
2025-01-10 13:23:51 +0530

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://cat-chat-2gnx.onrender.com');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messegeInp');
